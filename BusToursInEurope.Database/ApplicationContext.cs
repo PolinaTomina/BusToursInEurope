9365d8f8ba171ea4f87d0d111d3b04bfbc38b3e5
Polina Tomina <polina@HOME-PC>
2024-12-12 00:32:33 +0300

    Добавила свойства для моделей

--- BusToursInEurope.Database/ApplicationContext.cs
+++ BusToursInEurope.Database/ApplicationContext.cs
@@ -1,9 +1,25 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using BusToursInEurope.Core.Entites;
+using Microsoft.EntityFrameworkCore;
 
 namespace BusToursInEurope.Database
 {
     public class ApplicationContext : DbContext
     {
+        public DbSet<City> Cities => Set<City>();
+        public DbSet<Hotel> Hotels => Set<Hotel>();
+        public DbSet<Reservation> Reservations => Set<Reservation>();
+        public DbSet<Review> Reviews => Set<Review>();
+        public DbSet<Route> Routes => Set<Route>();
+        public DbSet<Tour> Tours => Set<Tour>();
+        public DbSet<Transport> Transports => Set<Transport>();
+        public DbSet<User> Users => Set<User>();
+        public DbSet<WayPoint> WayPoints => Set<WayPoint>();
 
+        public ApplicationContext() => Database.EnsureCreated();
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source=BusTourInEurope.db");
+        }
     }
 }
