2dded044e2b52b9a8195eb89e61a6d4dbe9643a8
AlexandrAshevsky <ashevskya@gmail.com>
2025-05-30 17:28:46 +0300

    fix

--- BusToursInEurope.Frontend/src/App.tsx
+++ BusToursInEurope.Frontend/src/App.tsx
@@ -6,31 +6,34 @@ import { AdminLayout } from './components/layout/AdminLayout/AdminLayout';
 import { AdminBusPage, AdminHotelsPage, AdminOrdersPage, AdminToursPage } from './pages/admin';
 import { AuthorizationPage, UserProfilePage } from './pages/user';
 import classes from './app.module.css'
+import { AuthProvider } from './components/common/Authentication/AuthenticationProvider';
 
 function App() {
   return(
     <div className={classes.app}>
-      <BrowserRouter>
-        <Routes>
-          <Route element={<DefaultLayout />}>
-            <Route path='/' element={<MainPage />} />
-            <Route path='main' element={<MainPage />} />
-            <Route path='about' element={<AboutUsPage />}/>
-            <Route path='authentication' element={<AuthorizationPage/>}/>
-            <Route path='tours' element={<ToursPage />} />
-            <Route path='tours/:id' element={<FullTourPage />} />
-            <Route path='profile' element={<UserProfilePage/>} />
-          </Route>
-          <Route path='/admin' element={<AdminLayout />}>
-            <Route path='buses' element={<AdminBusPage />} />
-            <Route path='hotels' element={<AdminHotelsPage />} />
-            <Route path='orders' element={<AdminOrdersPage />} />
-            <Route path='tours' element={<AdminToursPage />} />
-            <Route path='profile' element={<UserProfilePage/>} />
-            <Route path='users' element={<div></div>} />
-          </Route>
-        </Routes>
-      </BrowserRouter>
+      <AuthProvider>
+        <BrowserRouter>
+          <Routes>
+            <Route element={<DefaultLayout />}>
+              <Route path='/' element={<MainPage />} />
+              <Route path='main' element={<MainPage />} />
+              <Route path='about' element={<AboutUsPage />}/>
+              <Route path='authentication' element={<AuthorizationPage/>}/>
+              <Route path='tours' element={<ToursPage />} />
+              <Route path='tours/:id' element={<FullTourPage />} />
+              <Route path='profile' element={<UserProfilePage/>} />
+            </Route>
+            <Route path='/admin' element={<AdminLayout />}>
+              <Route path='buses' element={<AdminBusPage />} />
+              <Route path='hotels' element={<AdminHotelsPage />} />
+              <Route path='orders' element={<AdminOrdersPage />} />
+              <Route path='tours' element={<AdminToursPage />} />
+              <Route path='profile' element={<UserProfilePage/>} />
+              <Route path='users' element={<div></div>} />
+            </Route>
+          </Routes>
+        </BrowserRouter>
+      </AuthProvider>
     </div>
   )
 }
