4fef1fb754a35cb4b3494875207cd8cfa13ff2b8
Polina Tomina <polina@HOME-PC>
2025-06-03 17:40:31 +0300

    У админа теперь есть возможность блокировки и разблокировки пользователя (если у пользователя были брони, то при блокировки, его брони удаляются). Пользователь теперь может сохранять понравившиеся туры к себе в профиль и просматривать их

---
+++ BusToursInEurope.Application/AuthMiddleware.cs
@@ -0,0 +1,36 @@
+﻿using BusToursInEurope.Database;
+using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using System.Security.Claims;
+
+namespace BusToursInEurope.Application
+{
+    public class AuthMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public AuthMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext context, ApplicationContext dbContext)
+        {
+            var emailClaim = context.User.Claims.FirstOrDefault(c => c.Type == ClaimTypes.Name); // Ищем email в ClaimTypes.Name
+            var email = emailClaim?.Value;
+
+            if (!string.IsNullOrEmpty(email))
+            {
+                var user = await dbContext.Users.FirstOrDefaultAsync(u => u.Email == email);
+                if (user != null && user.IsBlocked)
+                {
+                    context.Response.StatusCode = 403;
+                    await context.Response.WriteAsync("Ваш аккаунт заблокирован.");
+                    return;
+                }
+            }
+
+            await _next(context);
+        }
+    }
+}
---
+++ BusToursInEurope/Controllers/AdminController.cs
@@ -0,0 +1,39 @@
+﻿using BusToursInEurope.Application.Contstants;
+using BusToursInEurope.Application.Interfaces;
+using BusToursInEurope.Application.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BusToursInEurope.Controllers
+{
+    [ApiController]
+    [Authorize(Roles = Role.Admin)]
+    [Route("/admin")]
+    public class AdminController : ControllerBase
+    {
+        private readonly IAdminService _adminService;
+
+        public AdminController(IAdminService adminService)
+        {
+            _adminService = adminService;
+        }
+
+        [HttpPost("block/{userId}")]
+        public async Task<IActionResult> BlockUser(int userId)
+        {
+            var result = await _adminService.BlockUser(userId);
+            if (!result) return NotFound("Пользователь не найден");
+
+            return Ok("Пользователь заблокирован");
+        }
+
+        [HttpPost("unblock/{userId}")]
+        public async Task<IActionResult> UnblockUser(int userId)
+        {
+            var result = await _adminService.UnblockUser(userId);
+            if (!result) return NotFound("Пользователь не найден");
+
+            return Ok("Пользователь разблокирован");
+        }
+    }
+}
